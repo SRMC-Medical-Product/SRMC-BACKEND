e71276bf1a8c79b80d79d05ffd9a4af5762d8def
Aravind Swaminathan <aravindofficial2203@gmail.com>
2022-01-24 12:00:30 +0530

    Celery beat setup

--- srmc/celery.py
+++ srmc/celery.py
@@ -3,6 +3,8 @@ from __future__ import absolute_import,unicode_literals
 import os
 from celery import Celery
 from django.conf import settings
+from celery.schedules import crontab
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE','srmc.settings')
 
 app=Celery('srmc')
@@ -12,7 +14,11 @@ app.conf.update(timezone="Asia/Kolkata")
 app.config_from_object(settings,namespace='CELERY')
 
 app.conf.beat_schedule={
-    
+    'send-mail-every-day-at-8':{
+        'task':'mainapp.tasks.test_func',
+        'schedule':crontab(hour=8,minute=15),
+        
+    }
 }
 app.autodiscover_tasks()
 
