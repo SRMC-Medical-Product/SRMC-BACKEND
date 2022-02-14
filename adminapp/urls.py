7cce4beee7b52118e74c2a575dad6bdc40df2d48
Rohith ND <ndrohith09@gmail.com>
2022-02-14 11:09:50 +0530

    adminapp view

---
+++ adminapp/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from adminapp.views import *
+
+urlpatterns = [
+
+    # --------------------------------- Admin User URLS ---------------------------------
+    path('login/' , AdminLogin.as_view() , name='admin-login'),
+    path('register/' , AdminRegister.as_view() , name='admin-register'),
+    path('all-get/' , AdminData.as_view() , name='admin-all-get'),
+    path('user-get/' , AdminUserGet.as_view() , name='admin-user-get'),
+    path('user-modify/' , AdminUserModify.as_view() , name='admin-user-modify'),
+    path('user-password-modify/' , AdminUserPasswordModify.as_view() , name='admin-user-password-modify'),
+
+
+]
